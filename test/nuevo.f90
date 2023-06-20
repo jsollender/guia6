79fb1e78d130eb902fb9df53064fc32e7a548511
jsollender <jsollender@mi.unc.edu.ar>
2023-06-20 16:39:33 -0300

    agregue comentarios

--- test/nuevo.f90
+++ test/nuevo.f90
@@ -7,12 +7,15 @@ implicit none
 contains
 
 	subroutine euler (a, b, N, alfa, w)
-	  
+	
 	  !definimos las variables que entran y salen
-	  	real(wp), intent(in)				:: a, b		!extremos del intervalo
-	  	real(wp), dimension(:), intent(in)	:: alfa	!condición inicial (t,y(t))
-	  	integer(il), intent(in)				:: N		!cantidad de intervalos
-	  	real(wp), dimension(:), intent(out)	:: w	!aprox. de solucion exacta
+	  	real(wp), intent(in)				:: a, b	!extremos del intervalo
+	  	real(wp), dimension(:), intent(in)	:: alfa	!condición inicial (t,y(t))	(*)
+	  	integer(il), intent(in)				:: N	!cantidad de intervalos
+	  	real(wp), dimension(:), intent(out)	:: w	!aprox. de solucion exacta	(*)
+	  
+	  !	(*) la dimensión viene desde afuera. Acá la dejo abierta así la subrutina se
+	  !		puede comer cualquier cosa
 	  
 	  !definimos las variables auxiliares
 	  	real(wp)						:: t, h	!ptos. del intervalo y su tamaño
@@ -28,7 +31,8 @@ contains
 	  		w = w + h * func(t,w)
 	  		t = a + i*h
 	  		print*, t, w(1), w(2)
-	  	end do
-	  	
+	  	end do	  	
 	end subroutine euler
+	
+	subroutine
 end module metodos
