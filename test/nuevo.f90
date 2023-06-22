fa276f3781428ead891cc38227dfc762d602c173
jsollender <jsollender@mi.unc.edu.ar>
2023-06-21 23:08:26 -0300

    DATAZOS

--- test/nuevo.f90
+++ test/nuevo.f90
@@ -22,20 +22,32 @@ contains
 	  	!		puede comer cualquier cosa
 	  
 	  !definimos las variables auxiliares
-	  	real(wp)						:: t, h	!ptos. del intervalo y su tamaño
-	  	integer(wp)						:: i	!para el do
+	  	real(wp)					:: t, h	!ptos. del intervalo y su tamaño
+	  	integer(il)					:: i	!para el do
+	  
+	  !definimos las variables para el archivo de datos
+	  	integer(il)					:: fu
+	  	character(20)				:: nombre_archivo
 	  
 	  !inicializamos las variables
 	  	t = a
 	  	w = alfa
 	  	h = (b-a)/N
+	  	nombre_archivo = 'datos_euler.dat'
 	  
+	  !abrimos archivo de datos		
+	  	open(newunit=fu, file=nombre_archivo)
+	  
+	  !escribimos el encabezado del archivo y la primer línea (cond. iniciales)
+	  	write(fu,*) '	  t			  aprox(1)		     aprox(2)'
+	  	write(fu,*) t, w(1), w(2)
+	  	
 	  !proceso
-	  	do i = 1, N+1, 1	
+	  	do i = 1, N, 1	
 	  		w = w + h * func(t,w)
 	  		t = a + i*h
-	  		!print de control
-	  		print*, t, w(1), w(2)
+	  	  !ahora escribimos el resto de datos 
+	  		write(fu,*) t, w(1), w(2)
 	  	end do	  	
 	end subroutine euler
 	
@@ -53,15 +65,27 @@ contains
 	  !definimos las variables auxiliares
 	  	real(wp)					 :: t, h			!ptos. del intervalo y su tamaño
 	  	real(wp), dimension(size(w)) :: k1, k2, k3, k4	!auxiliares
-	  	integer(wp)				 	 :: i				!para el do
+	  	integer(il)				 	 :: i				!para el do
+
+	  !definimos las variables para el archivo de datos
+	  	integer(il)					:: fu
+	  	character(20)				:: nombre_archivo
 	
 	  !inicializamos las variables
 	  	t = a
 	  	w = alfa
-	  	h = (b-a)/N	
+	  	h = (b-a)/N
+	  	nombre_archivo = 'datos_rk4.dat'
+	
+	!abrimos archivo de datos		
+	  	open(newunit=fu, file=nombre_archivo)
+	  
+	  !escribimos el encabezado del archivo y la primer línea (cond. iniciales)
+	  	write(fu,*) '	  t			  aprox(1)		     aprox(2)'
+	  	write(fu,*) t, w(1), w(2)
 	  	
 	  !proceso
-	  	do i = 1, N+1, 1
+	  	do i = 1, N, 1
 	  	  !inicializamos los k
 		  	k1 = h * func( t , w )
 		  	k2 = h * func( t + h * 0.5_wp , w + k1 * 0.5_wp ) 
@@ -71,8 +95,8 @@ contains
 	  		w = w + ( k1 + 2._wp * k2 + k3 * 2._wp + k4 )/6._wp
 	  		
 	  		t = a + i*h
-	  		!print de control
-	  		print*, t, w(1), w(2)
+	  	  !ahora escribimos el resto de datos 
+	  		write(fu,*) t, w(1), w(2)
 	  	end do
 	end subroutine rk4
 	
